032629abb9834345620020f56f7cf419a170a3a2
박권익 <kinowk@naver.com>
2025-12-25 22:44:11 +0900

    test: DisplayName 작성 가이드

--- src/test/java/com/example/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/com/example/cafekiosk/unit/CafeKioskTest.java
@@ -3,6 +3,7 @@ package com.example.cafekiosk.unit;
 import com.example.cafekiosk.unit.beverage.Americano;
 import com.example.cafekiosk.unit.beverage.Latte;
 import com.example.cafekiosk.unit.order.Order;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
@@ -21,6 +22,7 @@ class CafeKioskTest {
         System.out.println("담긴 음료: " + cafeKiosk.getBeverages().get(0).getName());
     }
 
+    @DisplayName("음료 1개를 추가하면, 주문 목록에 담긴다")
     @Test
     void add() {
         CafeKiosk cafeKiosk = new CafeKiosk();
@@ -77,8 +79,10 @@ class CafeKioskTest {
         assertThat(cafeKiosk.getBeverages()).isEmpty();
     }
 
+    @DisplayName("주문 목록에 담긴 상품의 총 금액을 계산할 수 있다.")
     @Test
     void calculateTotalPrice() {
+        // given
         CafeKiosk cafeKiosk = new CafeKiosk();
         Americano americano = new Americano();
         Latte latte = new Latte();
@@ -86,8 +90,10 @@ class CafeKioskTest {
         cafeKiosk.add(americano);
         cafeKiosk.add(latte);
 
+        // when
         int totalPrice = cafeKiosk.calculateTotalPrice();
 
+        // then
         assertThat(totalPrice).isEqualTo(5000);
     }
 
