5e5e9e8fdad899b0c92d37ef9dec51de20b28093
박권익 <kinowk@naver.com>
2025-12-26 18:44:23 +0900

    refactor: ProductService 리팩토링

--- src/main/java/com/example/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/com/example/cafekiosk/spring/api/service/product/ProductService.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +20,7 @@ public class ProductService {
 
         return sellingProducts.stream()
                 .map(ProductResponse::from)
-                .collect(Collectors.toList());
+                .toList();
     }
 
 }
