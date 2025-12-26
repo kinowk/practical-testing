4d7a6ccf69085526a08d07155c2f197101726f47
박권익 <kinowk@naver.com>
2025-12-26 17:07:38 +0900

    feat: Product 도메인 구현

---
+++ src/main/java/com/example/cafekiosk/spring/api/service/product/response/ProductResponse.java
@@ -0,0 +1,27 @@
+package com.example.cafekiosk.spring.api.service.product.response;
+
+import com.example.cafekiosk.spring.domain.product.Product;
+import com.example.cafekiosk.spring.domain.product.ProductSellingStatus;
+import com.example.cafekiosk.spring.domain.product.ProductType;
+
+public record ProductResponse(
+        Long id,
+        String productNumber,
+        ProductType type,
+        ProductSellingStatus sellingStatus,
+        String name,
+        int price
+) {
+
+    public static ProductResponse of(Product product) {
+        return new ProductResponse(
+                product.getId(),
+                product.getProductNumber(),
+                product.getType(),
+                product.getSellingStatus(),
+                product.getName(),
+                product.getPrice()
+        );
+    }
+
+}
