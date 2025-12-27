1709d80bd94cd349a04027421cc82e51ed5d89c7
박권익 <kinowk@naver.com>
2025-12-27 23:48:00 +0900

    feat: 주문 시 재고 차감

--- src/main/java/com/example/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/com/example/cafekiosk/spring/api/service/order/OrderService.java
@@ -6,12 +6,16 @@ import com.example.cafekiosk.spring.domain.order.Order;
 import com.example.cafekiosk.spring.domain.order.OrderRepository;
 import com.example.cafekiosk.spring.domain.product.Product;
 import com.example.cafekiosk.spring.domain.product.ProductRepository;
+import com.example.cafekiosk.spring.domain.product.ProductType;
+import com.example.cafekiosk.spring.domain.stock.Stock;
+import com.example.cafekiosk.spring.domain.stock.StockRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -22,11 +26,31 @@ public class OrderService {
 
     private final ProductRepository productRepository;
     private final OrderRepository orderRepository;
+    private final StockRepository stockRepository;
 
     @Transactional
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredAt) {
         List<String> productNumbers = request.productNumbers();
         List<Product> products = findProductsBy(productNumbers);
+
+        List<String> stockProductNumbers = products.stream()
+                .filter(product -> ProductType.containsStockType(product.getType()))
+                .map(Product::getProductNumber)
+                .toList();
+
+        List<Stock> stocks = stockRepository.findAllByProductNumberIn(productNumbers);
+        Map<String, Stock> stockMap = stocks.stream()
+                .collect(Collectors.toMap(Stock::getProductNumber, stock -> stock));
+
+        Map<String, Long> productCountingMap = stockProductNumbers.stream()
+                .collect(Collectors.groupingBy(p -> p, Collectors.counting()));
+
+        for (String stockProductNumber : new HashSet<>(stockProductNumbers)) {
+            Stock stock = stockMap.get(stockProductNumber);
+            int quantity = productCountingMap.get(stockProductNumber).intValue();
+
+            stock.deductQuantity(quantity);
+        }
 
         Order order = Order.create(products, registeredAt);
         Order savedOrder = orderRepository.save(order);
