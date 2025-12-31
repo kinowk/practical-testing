d8998b5c799ea32710a4ed70630d0d586f301826
박권익 <kinowk@naver.com>
2025-12-31 18:36:37 +0900

    refactor: 상품 생성 요청 Validation 추가

--- src/main/java/com/example/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
+++ src/main/java/com/example/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
@@ -3,12 +3,15 @@ package com.example.cafekiosk.spring.api.controller.product.dto.request;
 import com.example.cafekiosk.spring.domain.product.Product;
 import com.example.cafekiosk.spring.domain.product.ProductSellingStatus;
 import com.example.cafekiosk.spring.domain.product.ProductType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 
 public record ProductCreateRequest(
-        ProductType type,
-        ProductSellingStatus sellingStatus,
-        String name,
-        int price
+        @NotNull(message = "상품타입은 필수입니다.") ProductType type,
+        @NotNull(message = "상품 판매상태는 필수입니다") ProductSellingStatus sellingStatus,
+        @NotBlank(message = "상품명은 필수입니다.") String name,
+        @PositiveOrZero(message = "가격은 0보다 커야됩니다.") int price
 ) {
 
     public Product toEntity(String productNumber) {
