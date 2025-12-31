d51834d338380bcd1be900d705e77433ab92dfa3
박권익 <kinowk@naver.com>
2025-12-31 16:05:14 +0900

    feat: 상품 등록 기능 구현

--- src/main/java/com/example/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/com/example/cafekiosk/spring/api/service/product/ProductService.java
@@ -1,11 +1,13 @@
 package com.example.cafekiosk.spring.api.service.product;
 
+import com.example.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import com.example.cafekiosk.spring.api.service.product.response.ProductResponse;
 import com.example.cafekiosk.spring.domain.product.Product;
 import com.example.cafekiosk.spring.domain.product.ProductRepository;
 import com.example.cafekiosk.spring.domain.product.ProductSellingStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +17,17 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    @Transactional
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        String productNumber = createNextProductNumber();
+
+        Product product = request.toEntity(productNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.from(savedProduct);
+    }
+
+    @Transactional(readOnly = true)
     public List<ProductResponse> getSellingProducts() {
         List<Product> sellingProducts = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
 
@@ -23,4 +36,14 @@ public class ProductService {
                 .toList();
     }
 
+    @Transactional(readOnly = true)
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProductNumber();
+        if (latestProductNumber == null) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        return String.format("%03d", latestProductNumberInt + 1);
+    }
 }
