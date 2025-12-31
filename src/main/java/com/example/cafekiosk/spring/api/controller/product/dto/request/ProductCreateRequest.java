e3bbc757f7a03da6dd1e2f29795188aa0fb4ec3d
박권익 <kinowk@naver.com>
2025-12-31 19:04:53 +0900

    refactor: Validation 메시지 변경

--- src/main/java/com/example/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
+++ src/main/java/com/example/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
@@ -9,9 +9,9 @@ import jakarta.validation.constraints.PositiveOrZero;
 
 public record ProductCreateRequest(
         @NotNull(message = "상품타입은 필수입니다.") ProductType type,
-        @NotNull(message = "상품 판매상태는 필수입니다") ProductSellingStatus sellingStatus,
+        @NotNull(message = "상품 판매상태는 필수입니다.") ProductSellingStatus sellingStatus,
         @NotBlank(message = "상품명은 필수입니다.") String name,
-        @PositiveOrZero(message = "가격은 0보다 커야됩니다.") int price
+        @PositiveOrZero(message = "가격은 0원 이상이어야 됩니다.") int price
 ) {
 
     public Product toEntity(String productNumber) {
