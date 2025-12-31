5bb5cadbf1e9d578e84eca94d05cd101b9266e95
박권익 <kinowk@naver.com>
2025-12-31 16:52:46 +0900

    refactor: 상품 생성 Controller 리팩토링

--- src/main/java/com/example/cafekiosk/spring/api/controller/product/ProductController.java
+++ src/main/java/com/example/cafekiosk/spring/api/controller/product/ProductController.java
@@ -6,6 +6,7 @@ import com.example.cafekiosk.spring.api.service.product.response.ProductResponse
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,9 +18,8 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/api/v1/products/new")
-    public void createProduct(ProductCreateRequest request) {
-        productService.createProduct(request);
-
+    public ProductResponse createProduct(@RequestBody ProductCreateRequest request) {
+        return productService.createProduct(request);
     }
 
     @GetMapping("/api/v1/products/selling")
