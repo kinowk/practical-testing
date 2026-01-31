ea610289c7ee2f1c0511e287f01347738c5d2629
박권익 <kinowk@naver.com>
2026-01-31 17:15:02 +0900

    test: Mockito, BDDMockito, Spy

---
+++ src/test/java/com/example/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -0,0 +1,53 @@
+package com.example.cafekiosk.spring.api.service.mail;
+
+import com.example.cafekiosk.spring.client.MailSendClient;
+import com.example.cafekiosk.spring.domain.history.mail.MailSendHistory;
+import com.example.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class MailServiceTest {
+
+    @Mock
+    private MailSendClient mailSendClient;
+
+//    @Spy
+//    private MailSendClient mailSendClient;
+
+    @Mock
+    private MailSendHistoryRepository mailSendHistoryRepository;
+
+    @InjectMocks
+    private MailService mailService;
+
+    @DisplayName("메일 전송 테스트")
+    @Test
+    void sendMail() {
+        // given
+//        when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+//                .thenReturn(true);
+//        doReturn(true)
+//                .when(mailSendClient)
+//                .sendEmail(anyString(), anyString(), anyString(), anyString());
+        given(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+                .willReturn(true);
+
+        // when
+        boolean result = mailService.sendEmail("", "", "", "");
+
+        // then
+        assertThat(result).isTrue();
+        verify(mailSendHistoryRepository, times(1)).save(any(MailSendHistory.class));
+    }
+}
